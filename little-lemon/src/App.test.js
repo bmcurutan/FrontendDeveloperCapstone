c1476ff981135cc1ff58415008abd1635f888d66
Bianca <bmcurutan@gmail.com>
2025-06-05 20:52:18 -0700

    add tests

--- little-lemon/src/App.test.js
+++ little-lemon/src/App.test.js
@@ -1,9 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
+import { Routes, Route } from "react-router";
+import { fireEvent } from '@testing-library/react';
 import BookingPage from "./components/BookingPage";
+import ConfirmedBooking from "./components/ConfirmedBooking";
 import { initializeTimes, updateTimes } from "./components/BookingPage";
 
-describe("Booking", () => {
+describe("Booking form", () => {
   test("Renders BookingForm label", () => {
       render(
         <MemoryRouter>
@@ -32,5 +35,81 @@ describe("Booking", () => {
       "21:00",
       "21:30",
     ]);
+  });
+});
+
+describe("Input attributes", () => {
+  test('Date input has correct attributes', () => {
+    render(
+      <MemoryRouter>
+        <BookingPage />
+      </MemoryRouter>
+    );
+
+    const dateInput = screen.getByLabelText(/choose date/i);
+
+    expect(dateInput).toHaveAttribute('type', 'date');
+    expect(dateInput).toHaveAttribute('min');
+  });
+
+  test('Guests input has correct attributes', () => {
+    render(
+      <MemoryRouter>
+        <BookingPage />
+      </MemoryRouter>
+    );
+
+    const guestsInput = screen.getByLabelText(/number of guests/i); 
+
+    expect(guestsInput).toHaveAttribute('type', 'number');
+    expect(guestsInput).toHaveAttribute('min', '1');
+    expect(guestsInput).toHaveAttribute('max', '10');
+  });
+});
+
+describe("Form submission", () => {
+  test("Form submits with valid inputs", async () => {
+    render(
+      <MemoryRouter initialEntries={['/reservations']}>
+        <Routes>
+          <Route path="/reservations" element={<BookingPage />} />
+          <Route path="/confirmation" element={<ConfirmedBooking />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const dateString = "2025-06-05";
+    const dateInput = screen.getByLabelText(/choose date/i);
+    const timeInput = screen.getByLabelText(/choose time/i);
+    const guestsInput = screen.getByLabelText(/number of guests/i);
+    const submitButton = screen.getByRole('button', { name: /make your reservation/i });
+
+    fireEvent.change(dateInput, { target: { value: dateString } });
+    fireEvent.change(timeInput, { target: { value: '18:00' } });
+    fireEvent.change(guestsInput, { target: { value: '2' } });
+
+    fireEvent.click(submitButton);
+
+    expect(await screen.findByText(/booking confirmed/i)).toBeInTheDocument();
+  });
+
+  test("Form doesn't submit with invalid inputs", async () => {
+    render(
+      <MemoryRouter>
+        <BookingPage />
+      </MemoryRouter>
+    );
+
+    const dateString = "2025-06-05";
+    const dateInput = screen.getByLabelText(/choose date/i);
+    const guestsInput = screen.getByLabelText(/number of guests/i);
+    const submitButton = screen.getByRole('button', { name: /make your reservation/i });
+
+    fireEvent.change(dateInput, { target: { value: dateString } });
+    fireEvent.change(guestsInput, { target: { value: '0' } });
+
+    fireEvent.click(submitButton);
+
+    expect(screen.queryByText(/booking confirmed/i)).not.toBeInTheDocument();
   });
 });
